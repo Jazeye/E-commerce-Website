890b11a45c99d77956abd01580b3c5eb389e233d
MINNAL OP <Jazeye>
2023-03-19 13:38:40 +0530

    first create

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+function search() {
+    // Get the value of the search input
+    var searchValue = document.getElementById("search").value;
+    
+    // Do something with the search value
+    console.log("You searched for: " + searchValue);
+  }
