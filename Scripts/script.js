a14d0621d358fcbbe7186ac946270bc00b653fd8
MINNAL OP <Jazeye>
2023-03-21 00:31:21 +0530

    searchbar and slide

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,34 @@
 
+// navbar
 function navClick(){
   document.getElementById("navbar-menu-text").classList.toggle("show");
 }
-  
+  // navbar-end
+  // slide
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+// slide-end
