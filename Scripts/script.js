6230ee3f63ef7cc1b935205f1e0d6dc2af3e99f0
MINNAL OP <Jazeye>
2023-03-28 23:23:50 +0530

    page filled then json add then js update,add css

--- Scripts/script.js
+++ Scripts/script.js
@@ -38,3 +38,128 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 // slide-end
+// products json lines
+// window.addEventListener("load", function () {
+//   getJsonData("/data/products.json")
+//     .then((res) => {
+//       const products = res;
+//       let output = "";
+//       for (let item of products) {
+//         output += `
+//           <div class="container">
+//             <div class="content">
+//               <div class="card">
+//                 <div class="imgBox">
+//                   <img src="${item.image}"/>
+//                 </div>
+//                 <div class="contentbox">
+//                   <h3>${item.name}</h3>
+//                   <h2 class="price">${item.price}</h2>
+//                   <a href="#" class="buy">Buy Now</a>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         `;
+//       }
+//       document.querySelector("#products").innerHTML = output;
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// function getJsonData(url) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", url, true);
+//     xhr.responseType = "json";
+//     xhr.onload = () => {
+//       if (xhr.status === 200) {
+//         const data = xhr.response;
+//         resolve(data);
+//       } else {
+//         reject(new Error("Error fetching JSON data"));
+//       }
+//     };
+//     xhr.onerror = () => {
+//       reject(new Error("Error fetching JSON data"));
+//     };
+//     xhr.send();
+//   });
+// }
+// products json lines end
+window.addEventListener("load", function () {
+  getJsonData("/data/products.json")
+    .then((res) => {
+      const products = res;
+      const categories = getCategories(products);
+      let output = "";
+      for (let category of categories) {
+        const filteredProducts = products.filter(
+          (product) => product.category === category
+        );
+        output += `
+          <div class="category">
+            <h2>${category}</h2>
+            <div class="products">
+              ${generateProductsHtml(filteredProducts)}
+            </div>
+          </div>
+        `;
+      }
+      document.querySelector("#products").innerHTML = output;
+    })
+    .catch((err) => console.log(err));
+});
+
+function getCategories(products) {
+  const categories = [];
+  for (let product of products) {
+    if (!categories.includes(product.category)) {
+      categories.push(product.category);
+    }
+  }
+  return categories;
+}
+
+function generateProductsHtml(products) {
+  let output = "";
+  for (let item of products) {
+    output += `
+      <div class="container">
+        <div class="content">
+          <div class="card">
+            <div class="imgBox">
+              <img src="${item.image}"/>
+            </div>
+            <div class="contentbox">
+              <h3>${item.name}</h3>
+              <h2 class="price">${item.price}</h2>
+              <a href="#" class="buy">Buy Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+  return output;
+}
+
+function getJsonData(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const data = xhr.response;
+        resolve(data);
+      } else {
+        reject(new Error("Error fetching JSON data"));
+      }
+    };
+    xhr.onerror = () => {
+      reject(new Error("Error fetching JSON data"));
+    };
+    xhr.send();
+  });
+}
