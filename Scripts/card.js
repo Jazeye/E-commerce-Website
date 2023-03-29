da04436b494d70e6204b4876c17495d5f44059cb
MINNAL OP <Jazeye>
2023-03-29 20:20:35 +0530

    card json linked,pages create,css edited,create new js is called card.js

---
+++ Scripts/card.js
@@ -0,0 +1,80 @@
+// products json lines
+window.addEventListener("load", function () {
+    getJsonData("/data/products.json")
+      .then((res) => {
+        const products = res;
+        showProducts(res,"smartphone","#phoneproducts");
+        showProducts(res,"Laptop","#laptopproducts");
+        showProducts(res,"Accessories","#Accessoryproducts");
+        showProducts(res,"Men","#menproducts");
+        showProducts(res,"Women","#womenproducts");
+        showProducts(res,"Kids","#kidproducts");
+        
+        
+      })
+      .catch((err) => console.log(err));
+  });
+  
+  function getCategories(products) {
+    const categories = [];
+    for (let product of products) {
+      if (!categories.includes(product.category)) {
+        categories.push(product.category);
+      }
+    }
+    return categories;
+  }
+  
+  function generateProductsHtml(products) {
+    let output = "";
+    for (let item of products) {
+      output += `
+        <div class="container">
+          <div class="content">
+            <div class="card">
+              <div class="imgBox">
+                <img src="${item.image}"/>
+              </div>
+              <div class="contentbox">
+                <h3>${item.name}</h3>
+                <h2 class="price">${item.price}</h2>
+                <a href="#" class="buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    }
+    return output;
+  }
+  
+  function getJsonData(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+      xhr.responseType = "json";
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          const data = xhr.response;
+          resolve(data);
+        } else {
+          reject(new Error("Error fetching JSON data"));
+        }
+      };
+      xhr.onerror = () => {
+        reject(new Error("Error fetching JSON data"));
+      };
+      xhr.send();
+    });
+  }
+  
+  function showProducts(products,cat,_id) {
+    // body...
+    const filteredProducts = products.filter(
+            (product) => product.category === cat
+          );
+        let output = generateProductsHtml(filteredProducts);
+        document.querySelector(_id).innerHTML = output;
+  }
+  
+// products json lines end
