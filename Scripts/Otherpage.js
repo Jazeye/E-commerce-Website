adaaf7fbc4b9b6d84b23ba432964b715bc74c1c7
MINNAL OP <Jazeye>
2023-04-02 11:02:55 +0530

    add to cart text added

--- Scripts/Otherpage.js
+++ Scripts/Otherpage.js
@@ -83,52 +83,46 @@ function showProducts(products, cat, _id) {
 //Other page data show page json lines end
 //Get Id From Main Page
 let url = window.location.search;
-  let queryString = new URLSearchParams(url);
-  console.log(url);
-  const id = queryString.get("id");
+let queryString = new URLSearchParams(url);
+console.log(url);
+const id = queryString.get("id");
 // findjson lines
 window.addEventListener("load", function () {
-    getJsonData("/Data/OtherpageData.json")
-      .then((res) => {
-        const products = res;
-        const product = products.find((item) => item.id === id);
-        console.log(product);
-        loadProduct(product);
-      })
-      .catch((err) => console.log(err));
-  });
-  
-  //Showing Product Data
- function loadProduct(pro){
-    document.getElementById('pro-name').innerText = pro.name;
-    document.getElementById('pro-price').innerText = pro.price;
-    document.getElementById('pro-img').src = pro.image;
-    document.getElementById('pro-disc').innerText = pro.description;
-  }
-  
-  
-  
-  function getJsonData(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", url, true);
-      xhr.responseType = "json";
-      xhr.onload = () => {
-        if (xhr.status === 200) {
-          const data = xhr.response;
-          resolve(data);
-        } else {
-          reject(new Error("Error fetching JSON data"));
-        }
-      };
-      xhr.onerror = () => {
+  getJsonData("/Data/OtherpageData.json")
+    .then((res) => {
+      const products = res;
+      const product = products.find((item) => item.id === id);
+      console.log(product);
+      loadProduct(product);
+    })
+    .catch((err) => console.log(err));
+});
+
+//Showing Product Data
+function loadProduct(pro) {
+  document.getElementById("pro-name").innerText = pro.name;
+  document.getElementById("pro-price").innerText = pro.price;
+  document.getElementById("pro-img").src = pro.image;
+  document.getElementById("pro-disc").innerText = pro.description;
+}
+function getJsonData(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const data = xhr.response;
+        resolve(data);
+      } else {
         reject(new Error("Error fetching JSON data"));
-      };
-      xhr.send();
-    });
-  }
-  
-  
-  
-  // find json lines end
-  
+      }
+    };
+    xhr.onerror = () => {
+      reject(new Error("Error fetching JSON data"));
+    };
+    xhr.send();
+  });
+}
+
+// find json lines end
